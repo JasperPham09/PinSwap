566d8d86c0d3cd2955280d0747a3efd4d82eaa6d
Tuan Kiet <phamjasper09@gmail.com>
2025-03-05 21:02:13 +0700

    push

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 // app/_layout.tsx
 import { Tabs } from 'expo-router';
 import React from 'react';
+import { IconSymbol } from '@/components/ui/IconSymbol';
 
 const TabLayout = () => {
     return (
@@ -10,7 +11,7 @@ const TabLayout = () => {
                 options={{
                     tabBarLabel: 'Bản Đồ', // Label trên tab
                     title: 'Điểm Đổi Pin', // Title trên header
-                    // tabBarIcon: ({ color, focused }) => ( ... Icon cho tab ... ), // Tùy chỉnh icon
+                    // tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
                 }}
             />
             <Tabs.Screen
@@ -22,6 +23,18 @@ const TabLayout = () => {
                 }}
             />
             {/* TODO: Thêm các Tabs.Screen khác nếu cần (ví dụ: Trang yêu cầu đổi pin, trang cá nhân...) */}
+            <Tabs.Screen
+                name="(tabs)" // Tên route cho trang chủ (index.tsx)
+                options={{
+                    tabBarLabel: 'Home', // Label trên tab
+                    title: 'Trang chủ', // Title trên header
+                    tabBarIcon: ({ color, focused }) => {
+                        let iconName = focused ? 'home' : 'home-outline';
+                        return <IconSymbol name={iconName} size={24} color={color} />;
+                      }
+                      
+                }}
+            />
         </Tabs>
     );
 };
